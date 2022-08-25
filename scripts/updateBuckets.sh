0cb4858ddd6e639e8e4c63061d4c646c990b66c1
Jake Roberts <jroberts@topps.com>
2022-08-25 19:27:23 +0200

    final push

--- scripts/updateBuckets.sh
+++ scripts/updateBuckets.sh
@@ -4,32 +4,19 @@
 
 initialChangedFiles=$( echo $@ )
 
-echo "initially changed files " $initialChangedFiles
-
 englishFilePaths=()
 
 
 for file in $initialChangedFiles; do
-    echo "This is the file: " $file
     if [[ ${file:0:18} == "public/locales/en/" ]]; then
-        echo " adding to english path: " ${file:18}
         englishFilePaths+=("${file:18}")
     fi;
 done
-
-echo "This is the english files path: $englishFilePaths"
-echo "first argument: $1"
-
-# Get rid of the first value because it is going to be the parent directory of en/ede
-
-#  Loop through the GCP buckets, and add the files
 
 cd public/locales/en/ 
 
 for changedFile in ${englishFilePaths[@]}; do
     p=$1en/$changedFile 
-    echo THIS IS: "$p"
-    echo "this is the changed file: " $changedFile
     gsutil cp $changedFile $p || continue
 done;
 
@@ -38,7 +25,6 @@ cd ../../../
 germanFilePaths=()
 
 for file in $initialChangedFiles; do
-    echo "File again: $file"
     if [[ ${file:0:18} == "public/locales/de/" ]]; then
         germanFilePaths+=("${file:18}")
     fi;
@@ -48,7 +34,5 @@ cd public/locales/de/
 
 for changedFile in ${germanFilePaths[@]}; do
     p=$1de/$changedFile 
-    echo THIS IS: "$p"
-    echo "this is the changed file: " $changedFile
     gsutil cp $changedFile $p || continue
 done;
