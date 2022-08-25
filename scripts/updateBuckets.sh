cfd139e961c30befbede0f21027fe2eb37e45ec1
Jake Roberts <jroberts@topps.com>
2022-08-25 17:27:51 +0200

    changed the file paths to be more similar

--- scripts/updateBuckets.sh
+++ scripts/updateBuckets.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#  Run step for all changed files and put into Github Env
-configPaths=()
+#  Run step for all changed translation files files and put into Github Env
+englishFilePaths=()
 
 for file in $2; do
-    if [[ ${file:0:7} = "config/" ]]; then
-        configPaths+=(${file:7})
+    if [[ ${file:0:7} = "public/locales/en" ]]; then
+        englishFilePaths+=(${file:7})
     fi;
 done
 
-echo "This is the config path: $configPaths"
+echo "This is the config path: $englishFilePaths"
 echo "first argument: $1"
 
 buckets=( $1en/ $1de/)
@@ -21,8 +21,8 @@ echo "Buckets:  $buckets"
 
 cd config 
 
-for changedFile in $configPaths; do
-    for bucket in $1en/ $1de/; do
+for changedFile in $englishFilePaths; do
+    for bucket in $1en/; do
         echo "This is the bucket: $bucket "
         p=$bucket$changedFile 
         echo THIS IS: "$p"
@@ -31,3 +31,21 @@ for changedFile in $configPaths; do
     done;
 done;
 
+germanFilePaths=()
+
+for file in $2; do
+    if [[ ${file:0:7} = "public/locales/de" ]]; then
+        germanFilePaths+=(${file:7})
+    fi;
+done
+
+
+for changedFile in $germanFilePaths; do
+    for bucket in $1de/; do
+        echo "This is the bucket: $bucket "
+        p=$bucket$changedFile 
+        echo THIS IS: "$p"
+        echo "this is the changed file: " $changedFile
+        gsutil cp $changedFile $p || continue
+    done;
+done;
