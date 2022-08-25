8e6a81e16f4fdc3f47dd61698577f847957b1a51
Jake Roberts <jroberts@topps.com>
2022-08-25 17:32:30 +0200

    added backslash

--- scripts/updateBuckets.sh
+++ scripts/updateBuckets.sh
@@ -4,7 +4,7 @@
 englishFilePaths=()
 
 for file in $2; do
-    if [[ ${file:0:7} = "public/locales/en" ]]; then
+    if [[ ${file:0:7} = "public/locales/en/" ]]; then
         englishFilePaths+=(${file:7})
     fi;
 done
@@ -12,11 +12,8 @@ done
 echo "This is the config path: $englishFilePaths"
 echo "first argument: $1"
 
-buckets=( $1en/ $1de/)
-
 # Get rid of the first value because it is going to be the parent directory of en/ede
 
-echo "Buckets:  $buckets"
 #  Loop through the GCP buckets, and add the files
 
 cd config 
@@ -31,14 +28,17 @@ for changedFile in $englishFilePaths; do
     done;
 done;
 
+cd ..
+
 germanFilePaths=()
 
 for file in $2; do
-    if [[ ${file:0:7} = "public/locales/de" ]]; then
+    if [[ ${file:0:7} = "public/locales/de/" ]]; then
         germanFilePaths+=(${file:7})
     fi;
 done
 
+cd config
 
 for changedFile in $germanFilePaths; do
     for bucket in $1de/; do
